650192320568e7c6ac1bd0f1175dd596b77760af
joforikwakye <jeffkwakye4u@gmail.com>
2022-08-24 00:05:38 +0000

    an updated login method which returns the needed arguments

--- lib/services/http_service.dart
+++ lib/services/http_service.dart
@@ -3,15 +3,12 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_easyloading/flutter_easyloading.dart';
-import 'package:nex_vote_beta/screens/dashboard.dart';
 import 'package:nex_vote_beta/models/set_args.dart';
 
 class HttpService {
-  //responsible for making either a get or a post request to the server
   static final client = http.Client();
 
   static var loginUrl = Uri.parse('http://10.0.2.2:5000/login');
-  static var getStudentNameAndImage = Uri.parse('http://10.0.2.2:5000/student');
 
   static login(username, password, context) async {
     var headers = {
@@ -29,44 +26,64 @@ class HttpService {
       body: jsonEncode(body),
     );
 
+    var json;
+
     if (response.statusCode == 200) {
       //the response body of the post request
-      var json = jsonDecode(response.body);
+      print(response.body);
+      json = jsonDecode(response.body);
 
       //saving the status and student_id from the result dictionary to navigate to the dashboard
       var status = json['status'];
       var studentId = json['student_id'];
 
-      if (status == 'Login successful') {
+      if (json['status'] == 'Login successful') {
         await EasyLoading.showSuccess(json['status']);
-        fetchFirstnameAndImageUrl(studentId);
-        Navigator.pushReplacementNamed(context, '/dashboard');
-      }
-      if (json['status'] == 'Incorrect username or password') {
+        http.Response response2 = await client
+            .get(Uri.parse('http://10.0.2.2:5000/student/$studentId'));
+
+        if (response2.statusCode == 200) {
+          var json = jsonDecode(response2.body);
+
+          var firstName = json['first_name'];
+          var imageUrl = json['image'];
+
+          SetArgs args = SetArgs();
+          args.setFirstName(firstName);
+          args.setImageUrl(imageUrl);
+          Navigator.pushReplacementNamed(
+            context,
+            '/dashboard',
+            arguments: {
+              'name': args.getFirstName,
+              'url': args.getImageUrl,
+            },
+          );
+        }
+      } else if (json['status'] == 'Incorrect username or password') {
         await EasyLoading.showError(json['status']);
       } else if (json['status'] == 'Username does not exist') {
         await EasyLoading.showError(json['status']);
       }
     }
-  }
 
-//gets the student id and returns the first name and image url
-  static fetchFirstnameAndImageUrl(var studentId) async {
-    http.Response response =
-        await client.get(Uri.parse('http://10.0.2.2:5000/student/$studentId'));
+    // static void fetchFirstnameAndImageUrl(var studentId) async {
+    //   http.Response response =
+    //       await client.get(Uri.parse('http://10.0.2.2:5000/student/$studentId'));
 
-    if (response.statusCode == 200) {
-      var json = jsonDecode(response.body);
+    //   if (response.statusCode == 200) {
+    //     var json = jsonDecode(response.body);
 
-      var firstName = json['first_name'];
-      var imageUrl = json['image'];
+    //     var firstName = json['first_name'];
+    //     var imageUrl = json['image'];
 
-      SetArgs args = SetArgs();
-      args.setFirstName(firstName);
-      args.setImageUrl(imageUrl);
+    //     SetArgs args = SetArgs();
+    //     args.setFirstName(firstName);
+    //     args.setImageUrl(imageUrl);
 
-      // print("name: " + args.getFirstName);
-      // print("url: " + args.getImageUrl);
-    }
+    //     // print("name: " + firstName.toString());
+    //     // print("url: " + imageUrl.toString());
+    //   }
+    // }
   }
 }
