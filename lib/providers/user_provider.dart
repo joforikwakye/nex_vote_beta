b4c9f7aa4a9ab7927b1d4753b7d77f06e5485c73
joforikwakye <jeffkwakye4u@gmail.com>
2022-09-03 11:07:46 +0000

    a provider class for listening to changes in the app

---
+++ lib/providers/user_provider.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class UserProvider with ChangeNotifier {
+  bool votesSubmitted = false;
+  Map<String, String> userInfo = {"name": "", "imageUrl": ""};
+  final votes = {
+    "president": {"name": "", "imageUrl": ""},
+    "finSec": {"name": "", "imageUrl": ""},
+    "genSec": {"name": "", "imageUrl": ""},
+  };
+  int selectedPresident;
+  int selectedGenSec;
+  int selectedFinSec;
+
+  void addVote(String potfolio, String candidate, String imageUrl) {
+    // print(potfolio);
+    votes[potfolio]["name"] = candidate;
+    votes[potfolio]["imageUrl"] = imageUrl;
+    // print('=============================== $candidate : $imageUrl');
+    notifyListeners();
+  }
+
+  void initialiseProvider() {
+    votesSubmitted = false;
+  }
+
+  void changeSelectedValue(int newValue, String portfolio) {
+    switch (portfolio) {
+      case "president":
+        selectedPresident = newValue;
+        break;
+      case "finSec":
+        selectedFinSec = newValue;
+        break;
+      case "genSec":
+        selectedGenSec = newValue;
+        break;
+    }
+    notifyListeners();
+  }
+
+  void storeUserInfo(String name, String imageUrl) {
+    userInfo["name"] = name;
+    userInfo["imageUrl"] = imageUrl;
+    notifyListeners();
+  }
+
+  void votesSubmittedSuccessfuly() {
+    votesSubmitted = true;
+    notifyListeners();
+  }
+}
