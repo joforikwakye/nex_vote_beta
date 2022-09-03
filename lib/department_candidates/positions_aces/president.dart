a551842852b101a8ac178be37b62ec1c967434ec
joforikwakye <jeffkwakye4u@gmail.com>
2022-09-03 11:17:49 +0000

    called the custom radio tile in the president class

--- lib/department_candidates/positions_aces/president.dart
+++ lib/department_candidates/positions_aces/president.dart
@@ -5,6 +5,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:nex_vote_beta/models/get_presidents.dart';
+import 'package:nex_vote_beta/widgets/custom_radio_list.dart';
 
 class President extends StatefulWidget {
   const President({Key key}) : super(key: key);
@@ -29,6 +30,8 @@ class _PresidentState extends State<President> {
     return presidents;
   }
 
+  bool selected = false;
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
@@ -46,24 +49,10 @@ class _PresidentState extends State<President> {
                 child: Container(
                   decoration: const BoxDecoration(color: Color(0xffEDD9DB)),
                   padding: const EdgeInsets.all(10),
-                  child: RadioListTile(
-                    value: index,
-                    groupValue: selectedValue,
-                    title: Text(
-                      snapshot.data[index].firstName +
-                          " " +
-                          snapshot.data[index].imageUrl,
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    secondary: ClipRRect(
-                      borderRadius: BorderRadius.circular(27),
-                      child: Image.network(snapshot.data[index].lastName),
-                    ),
-                    onChanged: (int value) {
-                      setState(() {
-                        selectedValue = value;
-                      });
-                    },
+                  child: CustomRadioListTile(
+                    index: index,
+                    snapshot: snapshot,
+                    portfolio: "president",
                   ),
                 ),
               );
@@ -74,89 +63,3 @@ class _PresidentState extends State<President> {
     );
   }
 }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       padding: const EdgeInsets.symmetric(horizontal: 10),
-//       margin: const EdgeInsets.symmetric(horizontal: 10),
-//       child: ListView(
-//         padding: EdgeInsets.symmetric(vertical: 16),
-//         children: [
-//           const SizedBox(height: 50),
-
-//           //1ST
-//           Container(
-//             decoration: const BoxDecoration(color: Color(0xffEDD9DB)),
-//             padding: EdgeInsets.all(10),
-//             child: RadioListTile(
-//               value: 0,
-//               groupValue: selectedValue,
-//               title: Text(
-//                 'Rexford Machu',
-//                 style: TextStyle(fontWeight: FontWeight.bold),
-//               ),
-//               secondary: ClipRRect(
-//                 borderRadius: BorderRadius.circular(27),
-//                 child: Image.asset('assets/images/president1.jpeg'),
-//               ),
-//               onChanged: (int value) {
-//                 setState(() {
-//                   selectedValue = value;
-//                 });
-//               },
-//             ),
-//           ),
-//           const SizedBox(height: 20),
-
-//           //2ND
-//           Container(
-//             decoration: const BoxDecoration(color: Color(0xffEDD9DB)),
-//             padding: EdgeInsets.all(10),
-//             child: RadioListTile(
-//               value: 1,
-//               groupValue: selectedValue,
-//               title: Text(
-//                 'Marcel Nortey',
-//                 style: TextStyle(fontWeight: FontWeight.bold),
-//               ),
-//               secondary: ClipRRect(
-//                 borderRadius: BorderRadius.circular(27),
-//                 child: Image.asset('assets/images/president2.jpeg'),
-//               ),
-//               onChanged: (int value) {
-//                 setState(() {
-//                   selectedValue = value;
-//                 });
-//               },
-//             ),
-//           ),
-//           const SizedBox(height: 20),
-
-//           //3RD
-//           Container(
-//             decoration: const BoxDecoration(color: Color(0xffEDD9DB)),
-//             padding: EdgeInsets.all(10),
-//             child: RadioListTile(
-//               value: 2,
-//               groupValue: selectedValue,
-//               title: Text(
-//                 'Jeffrey Ofori Kwakye',
-//                 style: TextStyle(fontWeight: FontWeight.bold),
-//               ),
-//               secondary: ClipRRect(
-//                 borderRadius: BorderRadius.circular(27),
-//                 child: Image.asset('assets/images/mypic.JPG'),
-//               ),
-//               onChanged: (int value) {
-//                 setState(() {
-//                   selectedValue = value;
-//                 });
-//               },
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
---
+++ lib/widgets/custom_radio_list.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:nex_vote_beta/providers/user_provider.dart';
+import 'package:provider/provider.dart';
+
+import '../models/get_presidents.dart';
+
+class CustomRadioListTile extends StatelessWidget {
+  final int index;
+  final snapshot;
+  final String portfolio;
+
+  const CustomRadioListTile(
+      {Key key, this.index, this.snapshot, this.portfolio})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final groupValue = getGroupValue(portfolio, context);
+    return RadioListTile(
+      value: index,
+      groupValue: groupValue,
+      title: Text(
+        snapshot.data[index].firstName + " " + snapshot.data[index].imageUrl,
+        style: const TextStyle(fontWeight: FontWeight.bold),
+      ),
+      secondary: ClipRRect(
+        borderRadius: BorderRadius.circular(27),
+        child: Image.network(snapshot.data[index].lastName),
+      ),
+      onChanged: (value) => makeChanges(value, context),
+    );
+  }
+
+  int getGroupValue(portfolio, BuildContext context) {
+    switch (portfolio) {
+      case "president":
+        return context.watch<UserProvider>().selectedPresident;
+      case "finSec":
+        return context.watch<UserProvider>().selectedFinSec;
+      case "genSec":
+        return context.watch<UserProvider>().selectedGenSec;
+    }
+    return 0;
+  }
+
+  void makeChanges(int value, BuildContext context) {
+    Presidents selectedCandidate = snapshot.data[value];
+    context.read<UserProvider>().changeSelectedValue(value, portfolio);
+    context.read<UserProvider>().addVote(
+        portfolio,
+        "${selectedCandidate.firstName} ${selectedCandidate.imageUrl}",
+        selectedCandidate.lastName);
+  }
+}
