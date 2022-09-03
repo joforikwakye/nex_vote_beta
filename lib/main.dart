c1207a81d437ae16a03fd6df40ebe02793868c88
joforikwakye <jeffkwakye4u@gmail.com>
2022-09-03 11:47:14 +0000

    wrapped the entire material app with a nofifier listener

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:nex_vote_beta/department_candidates/biomed.dart';
 import 'package:nex_vote_beta/department_candidates/aces.dart';
 import 'package:nex_vote_beta/department_candidates/gesa.dart';
+import 'package:nex_vote_beta/providers/user_provider.dart';
 import 'package:nex_vote_beta/screens/dashboard.dart';
 import 'package:nex_vote_beta/screens/review.dart';
 import 'package:nex_vote_beta/screens/see_all.dart';
 import 'package:nex_vote_beta/screens/sign_in.dart';
 import 'package:nex_vote_beta/screens/success.dart';
+import 'package:provider/provider.dart';
 import 'screens/splash_screen.dart';
 
 void main() => runApp(const MyApp());
@@ -16,19 +18,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        '/dashboard': (context) => const Dashboard(),
-        '/see_all': (context) => const SeeAll(),
-        '/review': (context) => const ReviewScreen(),
-        '/gesa': (context) => Gesa(),
-        '/aces': (context) => const ACES(),
-        '/biomed': (context) => const BIOMED(),
-        '/success': (context) => const SuccessPage(),
-        '/sign_in': (context) => const SignIn(),
-      },
-      debugShowCheckedModeBanner: false,
-      home: const SplashScreen(),
+    return ChangeNotifierProvider(
+      create: (context) => UserProvider(),
+      child: MaterialApp(
+        routes: {
+          '/dashboard': (context) => const Dashboard(),
+          '/see_all': (context) => const SeeAll(),
+          '/review': (context) => const ReviewScreen(),
+          '/gesa': (context) => Gesa(),
+          '/aces': (context) => const ACES(),
+          '/biomed': (context) => const BIOMED(),
+          '/success': (context) => const SuccessPage(),
+          '/sign_in': (context) => const SignIn(),
+        },
+        debugShowCheckedModeBanner: false,
+        home: const SplashScreen(),
+      ),
     );
   }
 }
