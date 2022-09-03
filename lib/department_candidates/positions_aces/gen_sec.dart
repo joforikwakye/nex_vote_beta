662a61137eb922cd8a989c3756d4804c7566204f
joforikwakye <jeffkwakye4u@gmail.com>
2022-09-03 11:27:44 +0000

    using the custom radio list tile

--- lib/department_candidates/positions_aces/gen_sec.dart
+++ lib/department_candidates/positions_aces/gen_sec.dart
@@ -5,6 +5,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:nex_vote_beta/models/get_presidents.dart';
+import 'package:nex_vote_beta/widgets/custom_radio_list.dart';
 
 class GenSec extends StatefulWidget {
   const GenSec({Key key}) : super(key: key);
@@ -50,24 +51,10 @@ class _GenSecState extends State<GenSec> {
                 child: Container(
                   decoration: const BoxDecoration(color: Color(0xffEDD9DB)),
                   padding: const EdgeInsets.all(10),
-                  child: RadioListTile(
-                    value: index,
-                    groupValue: selectedValue,
-                    title: Text(
-                      snapshot.data[index].firstName +
-                          " " +
-                          snapshot.data[index].imageUrl,
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    secondary: ClipRRect(
-                      borderRadius: BorderRadius.circular(27),
-                      child: Image.network(snapshot.data[index].lastName),
-                    ),
-                    onChanged: (int value) {
-                      setState(() {
-                        selectedValue = value;
-                      });
-                    },
+                  child: CustomRadioListTile(
+                    index: index,
+                    snapshot: snapshot,
+                    portfolio: "genSec",
                   ),
                 ),
               );
@@ -77,91 +64,3 @@ class _GenSecState extends State<GenSec> {
     );
   }
 }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       padding: const EdgeInsets.symmetric(horizontal: 10),
-//       margin: const EdgeInsets.symmetric(horizontal: 10),
-//       child: ListView(
-//         padding: EdgeInsets.symmetric(vertical: 16),
-//         children: [
-//           const SizedBox(height: 50),
-
-//           //1ST
-//           Container(
-//             decoration: const BoxDecoration(color: Color(0xffEDD9DB)),
-//             padding: EdgeInsets.all(10),
-//             child: RadioListTile(
-//               value: 0,
-//               groupValue: selectedValue,
-//               title: Text(
-//                 'Rexford Machu',
-//                 style: TextStyle(fontWeight: FontWeight.bold),
-//               ),
-//               secondary: ClipRRect(
-//                 borderRadius: BorderRadius.circular(27),
-//                 child: Image.asset('assets/images/president1.jpeg'),
-//               ),
-//               onChanged: (int value) {
-//                 setState(() {
-//                   selectedValue = value;
-//                 });
-//               },
-//             ),
-//           ),
-//           const SizedBox(height: 20),
-
-//           //2ND
-//           Container(
-//             decoration: const BoxDecoration(color: Color(0xffEDD9DB)),
-//             padding: EdgeInsets.all(10),
-//             child: RadioListTile(
-//               value: 1,
-//               groupValue: selectedValue,
-//               title: Text(
-//                 'Marcel Nortey',
-//                 style: TextStyle(fontWeight: FontWeight.bold),
-//               ),
-//               secondary: ClipRRect(
-//                 borderRadius: BorderRadius.circular(27),
-//                 child: Image.asset('assets/images/president2.jpeg'),
-//               ),
-//               onChanged: (int value) {
-//                 setState(() {
-//                   selectedValue = value;
-//                 });
-//               },
-//             ),
-//           ),
-//           const SizedBox(height: 20),
-
-//           //3RD
-//           Container(
-//             decoration: const BoxDecoration(color: Color(0xffEDD9DB)),
-//             padding: EdgeInsets.all(10),
-//             child: RadioListTile(
-//               value: 2,
-//               groupValue: selectedValue,
-//               title: Text(
-//                 'Jeffrey Ofori Kwakye',
-//                 style: TextStyle(fontWeight: FontWeight.bold),
-//               ),
-//               secondary: ClipRRect(
-//                 borderRadius: BorderRadius.circular(27),
-//                 child: Image.asset('assets/images/mypic.JPG'),
-//               ),
-//               onChanged: (int value) {
-//                 setState(() {
-//                   selectedValue = value;
-//                 });
-//               },
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-
