60fda703ef45c79e6d1646aa56eb3728bf6e6941
joforikwakye <jeffkwakye4u@gmail.com>
2022-09-18 17:48:48 +0000

    removed the future call in the file to api_calls.dart

--- lib/department_candidates/positions_gesa/president.dart
+++ lib/department_candidates/positions_gesa/president.dart
@@ -1,10 +1,7 @@
 //DISPLAYING THE PRESIDENTIAL CANDIDATES FOR GESA
 
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:nex_vote_beta/models/get_presidents.dart';
-import 'package:http/http.dart' as http;
+import 'package:nex_vote_beta/services/api_calls.dart';
 import 'package:nex_vote_beta/widgets/custom_radio_list.dart';
 
 class PresidentGesa extends StatefulWidget {
@@ -17,27 +14,10 @@ class PresidentGesa extends StatefulWidget {
 class _PresidentGesaState extends State<PresidentGesa> {
   int selectedValue = 4;
 
-  Future getGesaPresidents() async {
-    var response =
-        await http.get(Uri.parse('http://10.0.2.2:5000/gesa_presidents'));
-    var jsonData = jsonDecode(response.body);
-
-    //maintaining this naming convention because the other classes literally have the same data
-    //but changed the function name tho
-    List<Presidents> presidents = [];
-    for (var pres in jsonData) {
-      Presidents president =
-          Presidents(pres['firstname'], pres['imageurl'], pres['lastname']);
-      presidents.add(president);
-    }
-
-    return presidents;
-  }
-
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: getGesaPresidents(),
+      future: ApiCalls.getGesaPresidents(),
       // ignore: missing_return
       builder: (context, snapshot) {
         if (snapshot.data == null) {
