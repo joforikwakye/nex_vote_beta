e35eb8ff9386a998e293a5c96a98ed5eebc616f1
joforikwakye <jeffkwakye4u@gmail.com>
2022-09-18 17:30:12 +0000

    added a function to make an api call to biomed_gensec and displayed the results dynamically

--- lib/department_candidates/positions_biomed/gen_sec.dart
+++ lib/department_candidates/positions_biomed/gen_sec.dart
@@ -1,6 +1,11 @@
 //DISPLAYING THE GENERAL SECRETARY CANDIDATES FOR BIOMED
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:nex_vote_beta/models/get_presidents.dart';
+import 'package:nex_vote_beta/widgets/custom_radio_list.dart';
+import 'package:http/http.dart' as http;
 
 class GenSecBiomed extends StatefulWidget {
   const GenSecBiomed({Key key}) : super(key: key);
@@ -12,88 +17,51 @@ class GenSecBiomed extends StatefulWidget {
 class _GenSecBiomedState extends State<GenSecBiomed> {
   int selectedValue = 4;
 
+  Future getBiomedGenSec() async {
+    var response =
+        await http.get(Uri.parse('http://10.0.2.2:5000/biomed_gensec'));
+    var jsonData = jsonDecode(response.body);
+
+    //maintaining this naming convention because the other classes literally have the same data
+    //but changed the function name tho
+    List<Presidents> presidents = [];
+    for (var pres in jsonData) {
+      Presidents president =
+          Presidents(pres['firstname'], pres['imageurl'], pres['lastname']);
+      presidents.add(president);
+    }
+
+    return presidents;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 10),
-      margin: const EdgeInsets.symmetric(horizontal: 10),
-      child: ListView(
-        padding: EdgeInsets.symmetric(vertical: 16),
-        children: [
-          const SizedBox(height: 50),
-
-          //1ST
-          Container(
-            decoration: const BoxDecoration(color: Color(0xffEDD9DB)),
-            padding: EdgeInsets.all(10),
-            child: RadioListTile(
-              value: 0,
-              groupValue: selectedValue,
-              title: Text(
-                'Rexford Machu',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              secondary: ClipRRect(
-                borderRadius: BorderRadius.circular(27),
-                child: Image.asset('assets/images/president1.jpeg'),
-              ),
-              onChanged: (int value) {
-                setState(() {
-                  selectedValue = value;
-                });
-              },
-            ),
-          ),
-          const SizedBox(height: 20),
-
-          //2ND
-          Container(
-            decoration: const BoxDecoration(color: Color(0xffEDD9DB)),
-            padding: EdgeInsets.all(10),
-            child: RadioListTile(
-              value: 1,
-              groupValue: selectedValue,
-              title: Text(
-                'Marcel Nortey',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              secondary: ClipRRect(
-                borderRadius: BorderRadius.circular(27),
-                child: Image.asset('assets/images/president2.jpeg'),
-              ),
-              onChanged: (int value) {
-                setState(() {
-                  selectedValue = value;
-                });
-              },
-            ),
-          ),
-          const SizedBox(height: 20),
-
-          //3RD
-          Container(
-            decoration: const BoxDecoration(color: Color(0xffEDD9DB)),
-            padding: EdgeInsets.all(10),
-            child: RadioListTile(
-              value: 2,
-              groupValue: selectedValue,
-              title: Text(
-                'Jeffrey Ofori Kwakye',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              secondary: ClipRRect(
-                borderRadius: BorderRadius.circular(27),
-                child: Image.asset('assets/images/mypic.JPG'),
-              ),
-              onChanged: (int value) {
-                setState(() {
-                  selectedValue = value;
-                });
-              },
-            ),
-          ),
-        ],
-      ),
+    return FutureBuilder(
+      future: getBiomedGenSec(),
+      // ignore: missing_return
+      builder: (context, snapshot) {
+        if (snapshot.data == null) {
+          return const Center(child: Text('Loading...'));
+        } else
+          // ignore: curly_braces_in_flow_control_structures
+          return ListView.builder(
+            itemCount: snapshot.data.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                  decoration: const BoxDecoration(color: Color(0xffEDD9DB)),
+                  padding: const EdgeInsets.all(10),
+                  child: CustomRadioListTile(
+                    index: index,
+                    snapshot: snapshot,
+                    portfolio: "finSec",
+                  ),
+                ),
+              );
+            },
+          );
+      },
     );
   }
 }
