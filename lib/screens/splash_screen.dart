31ca759d4d0be6ebc775ac7a27d9c08d314dc6d3
joforikwakye <jeffkwakye4u@gmail.com>
2022-09-22 10:48:08 +0000

    an updated splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,37 +1,84 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'sign_in.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key key}) : super(key: key);
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    startTimer(); // SplashScreen delay function
+  }
+
+  startTimer() async {
+    var duration = const Duration(seconds: 5);
+    return Timer(duration, loginRoute);
+  }
+
+  loginRoute() {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const SignIn(),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return AnimatedSplashScreen(
-      splash: Column(
-        children: const [
-          Text(
-            'NexVote',
-            style: TextStyle(
-              fontSize: 40,
-              fontWeight: FontWeight.bold,
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+                color: Color(0xff610b0c),
+                gradient: LinearGradient(
+                    colors: [(Color(0xff610b0c)), (Color(0xff610b0c))],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter)),
+          ),
+          const Center(
+              //alignment: Alignment.topCenter,
+              child: Align(
+            alignment: Alignment(0.00, 0.2),
+            child: Text(
+              "NexVote",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 025),
+            ),
+          )),
+          const Center(
+            child: Icon(
+              Icons.how_to_vote,
+              size: 120,
               color: Colors.white,
             ),
           ),
-          Text(
-            'Powered by Nexus',
-            style: TextStyle(
-              fontSize: 15,
-              color: Color(0xffFFEA0E),
+          Center(
+            child: Align(
+              alignment: const Alignment(0.00, 0.8),
+              child: Text(
+                "POWERED BY NEXUS",
+                style: GoogleFonts.roboto(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                  color: Color(0xffFFEA0E),
+                ),
+              ),
             ),
-          ),
+          )
         ],
       ),
-
-      //i think wrapper for come
-      nextScreen: const SignIn(),
-      splashTransition: SplashTransition.slideTransition,
-      backgroundColor: const Color(0xff610B0C),
     );
   }
 }
@@ -39,22 +86,21 @@ class SplashScreen extends StatelessWidget {
 
 
 
-
-//   //this function runs once each time the app is built
-//   void initState() {
-//     // TODO: implement initState
-//     super.initState();
-//     //navigateToSignIn();
-//   }
-
-// //an async function because of the await
-//   void navigateToSignIn() async {
-//     await Future.delayed(Duration(seconds: 5));
-//     Navigator.pushReplacement(
-//       context,
-//       MaterialPageRoute(
-//         //takes a builder and context to know which class to navigate to
-//         builder: (context) => SignIn(),
-//       ),
-//     );
-//   }
+      //   children: const [
+      //     Text(
+      //       'NexVote',
+      //       style: TextStyle(
+      //         fontSize: 40,
+      //         fontWeight: FontWeight.bold,
+      //         color: Colors.white,
+      //       ),
+      //     ),
+      //     Text(
+      //       'Powered by Nexus',
+      //       style: TextStyle(
+      //         fontSize: 15,
+      //         color: Color(0xffFFEA0E),
+      //       ),
+      //     ),
+      //   ],
+      // ),
